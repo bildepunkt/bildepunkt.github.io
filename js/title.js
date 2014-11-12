9a750c495036241dd50222b890f5c3e318873d66
Chris <chris@cp-graphics.com>
2014-11-11 19:27:57 -0800

    redo

--- js/title.js
+++ js/title.js
@@ -3,23 +3,32 @@ var title = (function() {
     var vpWidth;
     var vpHeight;
 
-    var init = function(id) {
+    var init = function(id, manualShow) {
         el = document.getElementById(id);
         el.style.position = 'absolute';
+
+        if (!manualShow) {
+            show();
+        }
 
         resize();
     };
 
+    var show = function() {
+        el.style.visibility = 'visible';
+    };
+
     var resize = function() {
-        vpWidth = window.innerWidth;
+        vpWidth  = window.innerWidth;
         vpHeight = window.innerHeight;
 
-        el.style.top = vpHeight / 2 - el.offsetHeight / 2 + 'px';
-        el.style.left = vpWidth / 2 - el.offsetWidth / 2 + 'px';
+        el.style.top  = (vpHeight / 2 - el.offsetHeight / 2) + 'px';
+        el.style.left = (vpWidth / 2 - el.offsetWidth / 2) + 'px';
     };
 
     return {
         init: init,
+        show: show,
         resize: resize
     };
 }());
