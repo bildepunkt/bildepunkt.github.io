033e3d091ca0ef2acd031d4b39e5396c9bd5bfbf
Chris Peters <bildepunkt@gmail.com>
2016-09-17 17:38:30 -0700

    add tags and images to deploy

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,11 @@ git commit -m "`date +%Y%m%d_%H%M%S`-prebuild";
 wintersmith build;
 
 git checkout --orphan build;
-#git rm -r --cached .;
 
 rm .gitignore;
 cp -r build/ .;
 
-git add CNAME archive.html articles/ css/ scripts/ style/ feed.xml index.html;
+git add CNAME articles/ css/ images/ scripts/ style/ tag/ feed.xml index.html;
 git commit -m `date +%Y%m%d_%H%M%S`;
 git push -f origin build:master;
 
