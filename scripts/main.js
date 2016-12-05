9d6e35e1f9f9cba73c84a2cdba2f04ef8358626e
Chris Peters <bildepunkt@gmail.com>
2016-12-04 23:08:44 -0800

    add bg bars w scrolling!!!

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,91 @@
     }
   }
 
+  class Bars {
+    constructor (c, w, h) {
+      this.canvas = c;
+      this.context = this.canvas.getContext("2d");
+      this.width = w;
+      this.height = h;
+      this.colors = ["#C9518A", "#39B7C4", "#586086"];
+      this.barHeight = 128;
+      this.ends = [];
+      this.canvas.width = this.width;
+      this.canvas.height = this.height;
+
+      this.context.globalAlpha = 0.1;
+
+      const barTotal = Math.ceil((w + h) / this.barHeight);
+
+      for (let i = 0; i < barTotal; i++) {
+        let end = Math.random() * (w / 2);
+
+        if (i === 0 || i % 2 === 0) {
+          end += (w / 2);
+        }
+
+        this.ends.push(end);
+      }
+    }
+
+    update (factor=0) {
+      let colorIndex = 0;
+      const halfWidth = this.width / 2;
+      const halfHeight = this.height / 2;
+
+      this.context.clearRect(0, 0, this.width, this.height);
+      this.context.save();
+      this.context.translate(halfWidth, halfHeight);
+      this.context.rotate(-45 * Math.PI / 180);
+      this.context.translate(-halfWidth, -this.height);
+
+      for (let i = 0; i < this.ends.length; i++) {
+        let end = this.ends[i];
+        this.context.fillStyle = this.colors[colorIndex];
+
+        if (end <= halfWidth) {
+          end += factor;
+          this.context.fillRect(
+            end - this.width,
+            i * this.barHeight,
+            this.width,
+            this.barHeight
+          );
+        } else {
+          end -= factor;
+          this.context.fillRect(
+            end,
+            i * this.barHeight,
+            end + this.width,
+            this.barHeight
+          );
+        }
+
+        colorIndex += 1;
+
+        if (colorIndex === this.colors.length) {
+          colorIndex = 0;
+        }
+      }
+
+      this.context.restore();
+    }
+  }
+
   const year = new Date().getFullYear();
   const logCss = "background-color:#586086; color:#39B7C4;";
+  const winWidth = window.innerWidth;
+  const winHeight = window.innerHeight;
+  let headerCanvas = document.querySelector("#headerCanvas");
+  let bars = new Bars(headerCanvas, winWidth, winHeight);
+  bars.update();
 
   new Section("header");
+  new Section("#about");
+
+  window.addEventListener("scroll", ()=> {
+    bars.update(window.scrollY);
+  }, false);
 
   console.log(`%c oh, hai! \n © ${year} BILDEPUNKT.COM | ALL RIGHTS RESERVED `, logCss);
 
