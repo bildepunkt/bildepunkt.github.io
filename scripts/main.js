2a23c7cac2de1b27154565f2cb256ba090f0594b
Chris Peters <cpeters@tableau.com>
2019-09-06 22:52:11 -0700

    improvements - remove full screen js sizing - clean up css - clean up svg animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,63 +1,19 @@
 (()=> {
-
-  class Section {
-    constructor (selector="section") {
-      this.section = document.querySelector(selector);
-    }
-
-    onResize () {
-      this.section.style.height = `${window.innerHeight}px`;
+  class Logger {
+    constructor (year) {
+      const logCss = "background-color:#586086; color:#39B7C4; font-size:16px";
+      console.log(`%c oh, hai! \n © ${year} BILDEPUNKT.COM | ALL RIGHTS RESERVED `, logCss);
     }
   }
 
-  class Logo {
-    constructor (svgSelector="#svgLogo") {
-      this.svg = document.querySelector(svgSelector);
-      this.svgGroup = document.querySelector(`${svgSelector} g`);
-    }
-
-    onResize () {
-      const w = window.innerWidth;
-      const h = window.innerHeight;
-      const offsetY = h < 360 ? 264 / 2 : 0;
-
-      this.svg.setAttribute("width", w);
-      this.svg.setAttribute("height", h);
-      this.svgGroup.setAttribute("transform", `translate(${w / 2}, ${h / 2 + offsetY})`);
-    }
-  }
-  
-  class DelayCallback {
-    constructor (callback, buffer=256) {
-      this.callback = callback;
-      this.timeoutId = null;
-      this.buffer = buffer;
-      this.canCall = false;
-    }
-
-    call () {
-      clearTimeout(this.timeoutId);
-      this.timeoutId = setTimeout(this.callback, this.buffer);
+  class Footer {
+    constructor(year) {
+      document.querySelector('footer span').innerHTML = year;
     }
   }
 
-  function windowResize () {
-    header.onResize();
-    logo.onResize();
-  }
-
-  let delayCallback = new DelayCallback(windowResize);
-  let logo = new Logo();
-  let header = new Section("header");
-  let delayCall = delayCallback.call.bind(delayCallback);
-
-  windowResize();
-
-  window.addEventListener("resize", delayCall, false);
-  window.addEventListener("orientationchange", delayCall, false);
-
-  const logCss = "background-color:#586086; color:#39B7C4; font-size:16px";
   const year = new Date().getFullYear();
-  console.log(`%c oh, hai! \n © ${year} BILDEPUNKT.COM | ALL RIGHTS RESERVED `, logCss);
+  new Logger(year);
+  new Footer(year);
 
 }).call(this);
